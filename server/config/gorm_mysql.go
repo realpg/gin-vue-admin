9d86ac018898dd0dd40895a326c65663d5e08058
realpg <realpg@163.com>
2024-03-20 22:11:57 +0800

    支持 unix socket方式连接mysql数据库

--- server/config/gorm_mysql.go
+++ server/config/gorm_mysql.go
@@ -1,10 +1,16 @@
 package config
 
+import "strings"
+
 type Mysql struct {
 	GeneralDB `yaml:",inline" mapstructure:",squash"`
 }
 
 func (m *Mysql) Dsn() string {
+	if strings.HasPrefix(m.Path, "unix:") {
+		//兼容某些newbie不注意 配置的unix:和socket path之间多了空格 或者尾部有多余空格
+		return m.Username + ":" + m.Password + "@unix(" + strings.TrimSpace(strings.TrimPrefix(m.Path, "unix:")) + ")/" + m.Dbname + "?" + m.Config
+	}
 	return m.Username + ":" + m.Password + "@tcp(" + m.Path + ":" + m.Port + ")/" + m.Dbname + "?" + m.Config
 }
 
